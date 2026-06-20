986eee8c57e23d4ff9b5a5a7e59927fabca86ea9
7889545 <1144781498@qq.com>
2026-06-21 01:56:49 +0800

    Set GamePackageName to com.yydwlxqcy02.cs

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -1,10 +1,8 @@
-//
 // Created by Perfare on 2020/7/4.
-//
 
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.yydwlxqcy02.cs"
 
-#endif //ZYGISK_IL2CPPDUMPER_GAME_H
+#endif // ZYGISK_IL2CPPDUMPER_GAME_H
